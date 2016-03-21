17b24b028f24c98d4fe97f44d77f01c0a3f34327
sam1390 <sam13_90@hotmail.com>
2016-03-21 17:22:59 -0400

    Added files via upload

---
+++ ShotCounter/ShotTest1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ShotTest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PuckPos = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.PuckVel = new System.Windows.Forms.Label();
+            this.Slope = new System.Windows.Forms.Label();
+            this.Shots = new System.Windows.Forms.Label();
+            this.Shot2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PuckPos
+            // 
+            this.PuckPos.AutoSize = true;
+            this.PuckPos.Location = new System.Drawing.Point(12, 9);
+            this.PuckPos.Name = "PuckPos";
+            this.PuckPos.Size = new System.Drawing.Size(35, 13);
+            this.PuckPos.TabIndex = 0;
+            this.PuckPos.Text = "label1";
+            this.PuckPos.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // PuckVel
+            // 
+            this.PuckVel.AutoSize = true;
+            this.PuckVel.Location = new System.Drawing.Point(12, 33);
+            this.PuckVel.Name = "PuckVel";
+            this.PuckVel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.PuckVel.Size = new System.Drawing.Size(35, 13);
+            this.PuckVel.TabIndex = 1;
+            this.PuckVel.Text = "label1";
+            // 
+            // Slope
+            // 
+            this.Slope.AutoSize = true;
+            this.Slope.Location = new System.Drawing.Point(12, 56);
+            this.Slope.Name = "Slope";
+            this.Slope.Size = new System.Drawing.Size(35, 13);
+            this.Slope.TabIndex = 2;
+            this.Slope.Text = "label1";
+            // 
+            // Shots
+            // 
+            this.Shots.AutoSize = true;
+            this.Shots.Location = new System.Drawing.Point(15, 110);
+            this.Shots.Name = "Shots";
+            this.Shots.Size = new System.Drawing.Size(35, 13);
+            this.Shots.TabIndex = 3;
+            this.Shots.Text = "label1";
+            // 
+            // Shot2
+            // 
+            this.Shot2.AutoSize = true;
+            this.Shot2.Location = new System.Drawing.Point(15, 132);
+            this.Shot2.Name = "Shot2";
+            this.Shot2.Size = new System.Drawing.Size(35, 13);
+            this.Shot2.TabIndex = 4;
+            this.Shot2.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Shot2);
+            this.Controls.Add(this.Shots);
+            this.Controls.Add(this.Slope);
+            this.Controls.Add(this.PuckVel);
+            this.Controls.Add(this.PuckPos);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PuckPos;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label PuckVel;
+        private System.Windows.Forms.Label Slope;
+        private System.Windows.Forms.Label Shots;
+        private System.Windows.Forms.Label Shot2;
+
+    }
+}
+
---
+++ ShotCounter/ShotTest1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HockeyEditor;
+
+namespace ShotTest1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            MemoryEditor.Init(false);
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        static int ShotCounterR = 0, ShotCounterB = 0;
+        static Boolean shot = false;
+        static float Posx=0, Posy=0, Posz=0;
+        static Boolean wrote = false;
+
+        static string Puckonnet()
+        {
+            float Velx = Puck.Position.X - Posx;
+            float Vely = Puck.Position.Y - Posy;
+            float Velz = Puck.Position.Z - Posz;
+            Posx = Puck.Position.X;
+            Posy = Puck.Position.Y;
+            Posz = Puck.Position.Z;
+
+            float Time = 0, x = 0, y = 0;
+            String ShotOnNet = "false";
+            int red = 0;
+            if (Velz < 0)
+            {
+                Time = (4 - Puck.Position.Z) / Velz;
+                red = 1; // blue
+            }
+
+            if (Velz > 0)
+            {
+                Time = (57 - Puck.Position.Z) / Velz;
+                red = 2; // red
+            }
+
+
+            x = Puck.Position.X + Velx * Time;
+            y = Puck.Position.Y + Vely * Time;
+            if (x > 13.65 && x < 16.35 && red == 1) // blue
+            {
+                if (y < .85)
+                {
+                    ShotOnNet = "true";
+                    if (Puck.Position.Z < 8 && Puck.Position.Z > 3 && Puck.Position.X < 19 && Puck.Position.X > 11)
+                    {
+                        if (shot == false && GameInfo.GameTime > 0 && GameInfo.StopTime == 0)
+                        {
+                            ShotCounterB++;
+                            shot = true;
+                        }
+                    }
+                }
+            }
+            else if (x > 13.65 && x < 16.35 && red == 2) // red
+            {
+                if (y < .85)
+                {
+                    ShotOnNet = "true";
+                    if (Puck.Position.Z > 53 && Puck.Position.Z < 58 && Puck.Position.X < 19 && Puck.Position.X > 11)
+                    {
+                        if (shot == false && GameInfo.GameTime > 0 && GameInfo.StopTime == 0)
+                        {
+                            ShotCounterR++;
+                            shot = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ShotOnNet = "False";
+                shot = false;
+            }
+            
+                return ShotOnNet;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            PuckPos.Text = "PuckPos: " + (string)Puck.Position.ToString();
+            PuckVel.Text = "GameTime: " + PlayerManager.LocalPlayer.StickPosition;
+            Slope.Text = "Shot On Net: " + Puckonnet();
+            Shots.Text = "Blue Shots: " + ShotCounterR;
+            Shot2.Text = "Red Shots: " + ShotCounterB;
+            if (GameInfo.Period == 0)
+            {
+                ShotCounterB = 0;
+                ShotCounterR = 0;
+                wrote = false;
+            }
+            if (GameInfo.GameOver == 1 && wrote == false)
+            {
+                Writer write = new Writer();
+                write.WriteFile("Red Shots: " + ShotCounterR + " Blue Shots: " + ShotCounterB);
+                wrote = true;
+            }
+        }
+
+
+
+
+    }
+}
